227778be4defb42a38a6a89eb89976c313c507c8
LAPTOP-3FRK1KHD\gjsxj <hackjun97@naver.com>
2021-05-30 16:35:02 +0900

    푸드트럭 등록 및 조회 부분 추가

--- src/main/java/kr/co/fos/foodtruck/FoodtruckController.java
+++ src/main/java/kr/co/fos/foodtruck/FoodtruckController.java
@@ -1,10 +1,15 @@
 package kr.co.fos.foodtruck;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,14 +20,27 @@ import kr.co.fos.member.Member;
 @RestController
 @RequestMapping("/foodtruck")
 public class FoodtruckController {
+	@Autowired
+	private FoodtruckServiceImple foodtruckService;
+	
 	@PostMapping
-	public ResponseEntity<Object> doFoodtruckRegister(Member member, Foodtruck foodtruck, @RequestParam MultipartFile attach) {
-		return null;
+	public ResponseEntity<Object> doFoodtruckRegister(@RequestBody Member member) {
+		
+		// 1. memberServiece.memberRegister(member);
+		foodtruckService.foodtruckRegister(member.getFoodtruck());
+		
+		foodtruckService.foodtruckRegister(new Foodtruck());
+		
+		return ResponseEntity.status(HttpStatus.OK).body(member);
 	}
 	
 	@GetMapping
 	public ResponseEntity<Object> doFoodTruckListInquiry(Foodtruck foodtruck) {
-		return null;
+//		Foodtruck foodtruck = new Foodtruck();
+//		foodtruck.setName(name);
+		List<Foodtruck> list = foodtruckService.foodtruckListInquiry(foodtruck);
+		
+		return ResponseEntity.status(HttpStatus.OK).body(list);
 	}
 	
 	@GetMapping("/{no}")
